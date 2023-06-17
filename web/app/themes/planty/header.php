5b6919b8de979109636ac0de9ab47c7f9389cc6a
khalid khatkhour <kharkhour@hotmail.fr>
2023-06-17 15:59:45 +0200

    css-responsive

--- web/app/themes/planty/header.php
+++ web/app/themes/planty/header.php
@@ -7,7 +7,7 @@
   <meta name="description" content="Planty, c’est la première boisson énergisante composée
    à 100 % de produits naturels. Avec son goût frais et fruité, retrouvez votre énergie grâce
     aux plantes riches en vitamine B2, à n’importe quel moment de la journée.">
-  <meta name="viewport" content="width=1440px">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Syne:wght@400;500;600;700;800&display=swap" rel="stylesheet">
